4faaf64b7cfaa8031962246b85d4c9e3c94f4038
TurBoss <j.l.toledano.l@gmail.com>
2017-03-03 17:03:44 +0100

    reorganize files

--- menu.py
+++ menu.py
@@ -12,13 +12,18 @@ import sdl2.ext
 import sdl2.sdlttf
 
 from const import WindowSize
+from input import Input
+from game import Game
 
+FPS = 60  # units.FPS
+MAX_FRAME_TIME = int(5 * (1000 / FPS))
 RESOURCES = sdl2.ext.Resources(__file__, 'resources')
 
 
 class Menu:
-    def __init__(self, renderer):
-        self.renderer = renderer
+    def __init__(self, window):
+        self.window = window
+        self.renderer = window.renderer
 
         self.menu_bg = RESOURCES.get_path("menu_bg.png")
         self.menu_cursor = RESOURCES.get_path("menu_cursor.png")
@@ -31,21 +36,61 @@ class Menu:
         self.cursor_position = [0, 0]
         self.cursor_sprite_size = 64
 
-        self.menu_bg_sprite = self.init_sprite(self.menu_bg)
-        self.menu_cursor_sprite = self.init_sprite(self.menu_cursor)
-
-    def init_sprite(self, file_path):
-        sprite_surface = self.factory.from_image(file_path)
-        return sprite_surface
+        self.menu_bg_sprite = self.factory.from_image(self.menu_bg)
+        self.menu_cursor_sprite = self.factory.from_image(self.menu_cursor)
 
     def update(self, elapsed_time):
         pass
+
+    def run(self):
+
+        menu_input = Input()
+
+        running = True
+        last_update_time = SDL_GetTicks()  # units.MS
+
+        while running:
+            start_time = SDL_GetTicks()  # units.MS
+
+            menu_input.begin_new_frame()
+            menu_events = sdl2.ext.get_events()
+
+            for event in menu_events:
+                if event.type == SDL_KEYDOWN:
+                    menu_input.key_down_event(event)
+
+                elif event.type == SDL_KEYUP:
+                    menu_input.key_up_event(event)
+
+                elif event.type == SDL_QUIT:
+                    running = False
+                    break
+
+            # Exit
+            if menu_input.was_key_pressed(SDLK_ESCAPE):
+                running = False
+
+            current_time = SDL_GetTicks()  # units.MS
+            elapsed_time = current_time - last_update_time  # units.MS
+
+            self.update(min(elapsed_time, MAX_FRAME_TIME));
+
+            last_update_time = current_time
+
+            self.draw()
+            self.window.refresh()
+
+            # This loop lasts 1/60th of a second, or 1000/60th ms
+            ms_per_frame = 1000 // FPS  # units.MS
+            elapsed_time = SDL_GetTicks() - start_time  # units.MS
+            if elapsed_time < ms_per_frame:
+                SDL_Delay(ms_per_frame - elapsed_time)
 
     def draw(self):
 
         renderer = self.renderer.renderer
 
-        sprite_size = self.cursor_sprite_size
+        cursor_size = self.cursor_sprite_size
 
         menu_bg = self.menu_bg_sprite
         menu_cursor = self.menu_cursor_sprite
@@ -54,8 +99,8 @@ class Menu:
 
         bg_src_rect.x = 0
         bg_src_rect.y = 0
-        bg_src_rect.w = sprite_size
-        bg_src_rect.h = sprite_size
+        bg_src_rect.w = menu_bg.size[0]
+        bg_src_rect.h = menu_bg.size[1]
 
         bg_dest_rect = SDL_Rect()
 
@@ -64,6 +109,7 @@ class Menu:
         bg_dest_rect.w = WindowSize.WIDTH
         bg_dest_rect.h = WindowSize.HEIGHT
 
+        """
         cursor_src_rect = SDL_Rect()
 
         cursor_src_rect.x = 0
@@ -77,6 +123,7 @@ class Menu:
         cursor_dest_rect.y = int((WindowSize.HEIGHT / 2) - (sprite_size / 2))
         cursor_dest_rect.w = sprite_size
         cursor_dest_rect.h = sprite_size
+        """
 
         render.SDL_RenderCopy(renderer, menu_bg.texture, bg_src_rect, bg_dest_rect)
-        render.SDL_RenderCopy(renderer, menu_cursor.texture, cursor_src_rect, cursor_dest_rect)
+        # render.SDL_RenderCopy(renderer, menu_cursor.texture, cursor_src_rect, cursor_dest_rect)
