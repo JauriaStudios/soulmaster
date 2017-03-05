44316b84501ea36962a43fde6d812f972ee9cdcd
TurBoss <j.l.toledano.l@gmail.com>
2017-03-05 20:55:17 +0100

    MapLayers

--- map.py
+++ map.py
@@ -34,29 +34,66 @@ class TiledRenderer(object):
         for k, v in self.tmx_data.tile_properties.items():
             logger.info("{0}\t{1}".format(k, v))
 
-    def render_tile_layer(self, layer):
+    def render_tile_layer(self, layer, level):
         # deref these heavily used references for speed
         window_x = WindowSize.WIDTH
         window_y = WindowSize.HEIGHT
+
+        renderer = self.renderer.renderer
+
         tw = self.tmx_data.tilewidth
         th = self.tmx_data.tileheight
-        renderer = self.renderer.renderer
+        pos = self.pos
+
         dest = sdl2.rect.SDL_Rect(0, 0, tw, th + 96)
         rce = sdl2.SDL_RenderCopyEx
 
-        # iterate over the tiles in the layer
-        for x, y, tile in layer.tiles():
-            texture, src, flip = tile
-            dest.x = int(((x - y) * tw / 2) + window_x / 2) + self.pos[0]
-            dest.y = int(((x + y) * th / 2) - window_y / 8) + self.pos[1]
-            angle = 90 if (flip & 4) else 0
-            rce(renderer, texture, src, dest, angle, None, flip)
+        background = layer.properties['background']
 
-    def render_map(self):
+        # iterate over the tiles in the layer
+        if (background == "true") and (level == "back"):
+            for x, y, tile in layer.tiles():
+
+                texture, src, flip = tile
+
+                dest.x = int(((x - y) * tw / 2) + window_x / 2) + pos[0]
+                dest.y = int(((x + y) * th / 2) - window_y / 8) + pos[1]
+
+                angle = 90 if (flip & 4) else 0
+
+                rce(renderer, texture, src, dest, angle, None, flip)
+
+        elif (background == "false") and (level == "up"):
+            for x, y, tile in layer.tiles():
+
+                texture, src, flip = tile
+
+                dest.x = int(((x - y) * tw / 2) + window_x / 2) + pos[0]
+                dest.y = int(((x + y) * th / 2) - window_y / 8) + pos[1]
+
+                angle = 90 if (flip & 4) else 0
+
+                if dest.y < (window_y / 2) - 72:
+                    rce(renderer, texture, src, dest, angle, None, flip)
+
+        elif (background == "false") and (level == "down"):
+            for x, y, tile in layer.tiles():
+
+                texture, src, flip = tile
+
+                dest.x = int(((x - y) * tw / 2) + window_x / 2) + pos[0]
+                dest.y = int(((x + y) * th / 2) - window_y / 8) + pos[1]
+
+                angle = 90 if (flip & 4) else 0
+
+                if dest.y >= (window_y / 2) - 72:
+                    rce(renderer, texture, src, dest, angle, None, flip)
+
+
+    def render_map(self, level):
         for layer in self.tmx_data.visible_layers:
-            # draw map tile layers
             if isinstance(layer, TiledTileLayer):
-                self.render_tile_layer(layer)
+                self.render_tile_layer(layer, level)
 
     def update(self, position, elapsed_time):
         self.pos = position
