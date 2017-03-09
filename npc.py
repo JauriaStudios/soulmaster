171d736ea18d9bfcb356da9bc662b3a02db7e794
TurBoss <j.l.toledano.l@gmail.com>
2017-03-09 19:34:28 +0100

    npc again

--- npc.py
+++ npc.py
@@ -9,14 +9,14 @@ if sys.platform == "win32":
     os.environ["PYSDL2_DLL_PATH"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libs')
 
 from sdl2 import SDL_Rect, SDL_RenderCopy
-import sdl2.ext
+from sdl2.ext import Resources, SpriteFactory, TEXTURE
 
 from const import WindowSize, Colors
 from utils import Timer, dice
 from db import DataBase
 from ui import Dialog
 
-RESOURCES = sdl2.ext.Resources(__file__, 'resources')
+RESOURCES = Resources(__file__, 'resources')
 
 
 class MotionType:
@@ -68,8 +68,8 @@ class NPC:
             RESOURCES.get_path("{0}_walking.png".format(self.name))
         ]
 
-        self.factory = sdl2.ext.SpriteFactory(
-            sdl2.ext.TEXTURE,
+        self.factory = SpriteFactory(
+            TEXTURE,
             renderer=self.renderer
         )
 
